b7c44a82034c94fe95d4d9a24045a1c430c852d1
SobreiraV <sobreirav@gmail.com>
2022-10-12 22:01:09 -0300

    01

--- Midi/main.js
+++ Midi/main.js
@@ -1,12 +1,17 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+function tocaSom(seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado')
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -16,7 +21,20 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    contador = contador + 1;
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code);
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+
 }
 
 
