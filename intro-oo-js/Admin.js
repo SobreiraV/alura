5c8fd1cd82b1c7ee93c95f8b92a3c6d628037d29
SobreiraV <sobreirav@gmail.com>
2022-11-20 14:06:10 -0300

    herança de classe

---
+++ intro-oo-js/Admin.js
@@ -0,0 +1,12 @@
+import User from "./User.js";
+
+class Admin extends User {
+    constructor(nome, email, nascimento, role = 'admin', ativo = true) {
+        super(nome, email, nascimento, role, ativo)
+        this.role = 'admin'
+    }
+}
+
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
+console.log(novoAdmin)
+console.log(novoAdmin.exibirInfos())
