9cfd46b8e6adcffb4ba226936832131c14135728
SobreiraV <sobreirav@gmail.com>
2022-11-19 22:48:52 -0300

    cli

--- node-lib-md/cli.js
+++ node-lib-md/cli.js
@@ -1,10 +1,45 @@
+import fs from 'fs';
 import pegaArquivo from "./index.js";
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
-async function processaTexto(caminho) {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(resultado);
+async function imprimeLista(valida, resultado, identificador = '') {
+
+    if (valida) {
+        await console.log('lista de links', identificador, listaValidada(resultado));
+    } else {
+    console.log('lista de links', identificador, resultado);
+    }
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
+
+    try {
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log('O arquivo não existe');
+            return;
+        }
+    }
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho);
+        imprimeLista(valida, resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(valida, lista, nomeDeArquivo);
+            /* console.log(`${caminho}/${nomeDeArquivo}`)
+            console.log(lista); */
+
+        })
+    }
+
 }
 
 processaTexto(caminho);
