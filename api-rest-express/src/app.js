75937685a8d10558365854ed4510d84fff3908e7
SobreiraV <sobreirav@gmail.com>
2022-11-27 15:03:22 -0300

    Controllers e routes

--- api-rest-express/src/app.js
+++ api-rest-express/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/livro.js";
+//import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão")); //para avisar no caso de erro
 db.once("open", () => {
@@ -8,49 +9,49 @@ db.once("open", () => {
 })
 
 const app = express();
-
 app.use(express.json()); //sem isso o que está chegando via post não é lido
+routes(app);
 
-/* const livros = [
-    {id: 1, "Titulo": "Senhor dos Aneis"},
-    {id: 2, "Titulo": "O Hobbit"},
-] */
+// const livros = [
+//     {id: 1, "Titulo": "Senhor dos Aneis"},
+//     {id: 2, "Titulo": "O Hobbit"},
+// ]
 
-app.get('/', (req, res) => {        //devolvendo uma mensagem para a rota '/' (rota vazia)
-    res.status(200).send('Curso de Node');
-})
+//app.get('/', (req, res) => {        //devolvendo uma mensagem para a rota '/' (rota vazia)
+//    res.status(200).send('Curso de Node');
+//})
 
-app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros);
-    })
-})
+// app.get('/livros', (req, res) => {  //enviado para o livroscontroller
+//     livros.find((err, livros) => {
+//         res.status(200).json(livros);
+//     })
+// })
 
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
+// app.get('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id);
+//     res.json(livros[index]);
+// })
 
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro cadastrado com sucesso');
-})
+// app.post('/livros', (req, res) => {
+//     livros.push(req.body);
+//     res.status(201).send('Livro cadastrado com sucesso');
+// })
 
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo; //com o index identificado ele altera o titulo
-    res.json(livros);
-})
+// app.put('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id);
+//     livros[index].titulo = req.body.titulo; //com o index identificado ele altera o titulo
+//     res.json(livros);
+// })
 
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1); //posição de inicil e quantos elementos serão excluidos
-    res.send(`Livro ${id} removido com sucesso`);
-})
+// app.delete('/livros/:id', (req, res) => {
+//     let {id} = req.params;
+//     let index = buscaLivro(id);
+//     livros.splice(index, 1); //posição de inicil e quantos elementos serão excluidos
+//     res.send(`Livro ${id} removido com sucesso`);
+// })
 
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id);
-}
+// function buscaLivro(id) {
+//     return livros.findIndex(livro => livro.id == id);
+// }
 
 export default app;
