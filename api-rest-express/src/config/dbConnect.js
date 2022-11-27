c53ee49607dada1043007ecbfddfa58a6dfa4408
SobreiraV <104680146+SobreiraV@users.noreply.github.com>
2022-11-26 23:09:12 -0300

    Delete

--- api-rest-express/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://vinicius:1234567890@cluster0.mqykj4l.mongodb.net/alura-node");
-
-let db = mongoose.connection;
-
-export default db;
